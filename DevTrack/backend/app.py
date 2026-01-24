309c957429c743a1554ee5af59924977a2ea918c
Ikifars <raphaelvictor016@gmail.com>
2026-01-24 06:43:10 -0300

    Update CORS configuration for API resources
    Atualização do CORS.

--- DevTrack/backend/app.py
+++ DevTrack/backend/app.py
@@ -11,7 +11,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-CORS(app, origins=["https://ikifars.github.io/Dev-TrackV2/"])
+#CORS(app, origins=["https://ikifars.github.io/Dev-TrackV2/"])
+CORS(app, resources={r"/api/*": {
+    "origins": ["https://ikifars.github.io"]
+}})
+
 
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET", "devtrack-dev")
 jwt = JWTManager(app)
